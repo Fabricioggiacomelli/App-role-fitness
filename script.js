c1e8c8c0caa116761b8713f26433f9122ab572fd
Fabricioggiacomelli <fabricioggiacomelli@gmail.com>
2026-03-17 10:44:37 -0300

    a

--- script.js
+++ script.js
@@ -3,29 +3,7 @@ function entrar() {
   document.getElementById("mainApp").classList.remove("hidden");
 }
 
-function trocar(tab) {
-  document.querySelectorAll(".tab").forEach(t => t.classList.remove("active"));
-  document.getElementById(tab).classList.add("active");
-
-  document.getElementById("title").innerText = 
-    tab === "home" ? "Início" :
-    tab === "mapa" ? "Mapa" :
-    tab === "chat" ? "Chat" : "Perfil";
-}
-
-function participar(btn) {
-  btn.innerText = "Confirmado ✅";
-  btn.style.background = "#555";
-}
-
-function enviar() {
-  const msg = document.getElementById("msg").value;
-  if (!msg) return;
-
-  const div = document.createElement("div");
-  div.className = "msg";
-  div.innerText = msg;
-
-  document.getElementById("chatBox").appendChild(div);
-  document.getElementById("msg").value = "";
+function go(page) {
+  document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
+  document.getElementById(page).classList.add("active");
 }
